68c258566c94d6421d67efbca01766fcf2a43042
Jack Weinbender <jweinbender@milligan.edu>
2013-01-30 16:43:19 -0500

    better approach using hard-code

--- ref-count.php
+++ ref-count.php
@@ -14,8 +14,7 @@ function ref_counter_menu( $wp_admin_bar ) {
 	$wp_admin_bar->add_menu( array(
 		'parent' => 'top-secondary',
 		'id'     => 'ref-count',
-		'title'  => 'Ref +',
-		'meta'   => array('class' => 'ref-count'),
+		'title'  => '<a id="ref-count">Ref +</a>',
 	) );
 
   $wp_admin_bar->add_node($args);
@@ -23,9 +22,28 @@ function ref_counter_menu( $wp_admin_bar ) {
 
 add_action( 'admin_bar_menu', 'ref_counter_menu', 999 );
 
-function ref_counter_enqueue(){
-	wp_enqueue_script( 'ga', plugin_dir_url( __file__ ) . 'ga.js', array('jquery'), '20130124', true );
-	wp_enqueue_script( 'ref-count', plugin_dir_url( __file__ ) . 'ref-count.js', array('jquery', 'ga'), '20130124', true );
-	wp_enqueue_style( 'ref-count', plugin_dir_url( __file__ ) . 'ref-count.css', '20130124' );
+function ref_counter_add_js(){
+	if(is_user_logged_in()){?>
+
+<script type="text/javascript">
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-4045275-3' ]);
+	
+	jQuery(document).ready(function(){
+
+		var category = 'ref-count';
+		var action = 'click';
+		var label = jQuery( '.display-name' ).html();
+
+		jQuery('#ref-count').click(function(){
+			_gaq.push(function(){
+				jQuery('#wp-admin-bar-ref-count').addClass('sent');
+			});
+			_gaq.push(['_trackEvent', category , action, label]);
+		});
+	});
+</script><?php }
 }
-add_action( 'admin_bar_init', 'ref_counter_enqueue' );
+
+add_action( 'wp_footer', 'ref_counter_add_js');
+add_action( 'admin_footer', 'ref_counter_add_js');
